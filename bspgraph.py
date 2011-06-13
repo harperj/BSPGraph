cb92c6a64053e6364e4bef1de430c79605496de1
Jonathan Harper <jwh376@msstate.edu>
2011-06-13 15:04:15 -0700

    Modified bspgraph.py and bspnode.py to add messaging and BSPGraph::run() to start the BSP process.

--- bspgraph.py
+++ bspgraph.py
@@ -4,7 +4,8 @@ import bspnode
 class BSPGraph:
     def __init__(self, num_nodes, step_function=None):
         self.num_nodes = num_nodes
-        self.nodes = [bspnode.BSPNode(node_id) for node_id in xrange(num_nodes)]
+        self.nodes = [bspnode.BSPNode(self, node_id) for node_id in xrange(num_nodes)]
+        self.master_inbox = []
         if step_function != None:
             self.setStep(step_function)
     
@@ -15,6 +16,18 @@ class BSPGraph:
     def setStep(self, step_function):
         for node in self.nodes:
             node.setStep(step_function)
+            
+    def run(self):
+        all_asleep = False
+        while not all_asleep:
+            all_asleep = True
+            self.deliverMessages()
+            for node in self.nodes:
+                if node.active:
+                    all_asleep = False
+                    node.step()
+        print "All nodes asleep, ending run."
+        
         
     def addUndirectedEdge(self, start, end, weight=1):
         self.nodes[start].addNeighbor(end)
@@ -28,3 +41,15 @@ class BSPGraph:
     def removeDuplicateNeighbors(self):
         for node in self.nodes:
             node.removeDuplicateNeighbors()
+            
+    def sendMessage(self, message):
+         self.master_inbox.append(message)
+         
+    def deliverMessages(self):
+        for message in self.master_inbox:
+            if self.nodes[message[0]].active == False:
+                #If a node is inactive and receives a message, it should be reactivated.
+                self.nodes[message[0]].active = True
+                
+            self.nodes[message[0]].inbox.append(message)
+        self.master_inbox = []
--- bspnode.py
+++ bspnode.py
@@ -1,7 +1,10 @@
 class BSPNode:
-    def __init__(self, node_id):
-        self.id = node_id
+    def __init__(self, parent_graph, node_id, active_default = True):
+        self.node_id = node_id
         self.neighbors = []
+        self.inbox = []
+        self.active = active_default
+        self.parent_graph = parent_graph
         
     def addNeighbor(self, neighbor_id):
         self.neighbors.append(neighbor_id)
@@ -11,8 +14,11 @@ class BSPNode:
         neighbor_set = set(self.neighbors)
         self.neighbors = list(neighbor_set)
         
-    def Step(self):
-        self.step_function()
+    def step(self):
+        self.step_function(self)
         
     def setStep(self, step_function):
         self.step_function = step_function
+        
+    def deactivate(self):
+        self.active = False
